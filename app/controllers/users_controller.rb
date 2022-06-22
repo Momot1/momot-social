d2e1278612459a82437e1f6307df7f3aa5b85633
Mark Momot <markmomot03@gmail.com>
2022-06-22 18:41:40 -0400

    Rebuild push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:first_name, :last_name, :password, :password_confirmation, :birthday, :email, :username)
+        params.permit(:first_name, :last_name, :password, :password_confirmation, :birthday, :email.downcase.gsub(/\s+/, ""), :username.downcase.gsub(/\s+/, ""))
     end
 end
