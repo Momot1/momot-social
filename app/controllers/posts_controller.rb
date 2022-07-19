a79c36846b148517178883c5880a28fd9460198b
Mark Momot <markmomot03@gmail.com>
2022-07-19 17:36:23 -0400

    Fixed bug where user not logged in could not search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-    skip_before_action :authorized, only: [:index]
+    skip_before_action :authorized, only: [:index, :search]
     def index
         render json: Post.all
     end
