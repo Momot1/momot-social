e435330ddb7d4af40ef0b64fc3d60d211d87c4a5
Mark Momot <markmomot03@gmail.com>
2022-08-13 13:38:06 -0400

    Fixed a bug where likes wouldn't work on user post's page

--- client/src/features/posts/UserPosts.js
+++ client/src/features/posts/UserPosts.js
@@ -1,19 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 import PostElement from "./PostElement";
 import "./css/home.css";
 
 function UserPosts() {
   const { username } = useParams();
-  const [posts, setPosts] = useState([]);
 
-  console.log(username);
-
-  useEffect(() => {
-    fetch(`/users/posts/${username}`)
-      .then((resp) => resp.json())
-      .then(setPosts);
-  }, []);
+  const posts = useSelector((state) => state.posts.posts.filter((post) => post.username === username));
 
   const postElements = posts.map((post) => <PostElement post={post} key={post.id} />);
 
